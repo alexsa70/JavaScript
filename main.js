88fcab58e257462ae4fd10e89ca69fa60a3827d9
alexsa70 <40318204+alexsa70@users.noreply.github.com>
2020-01-16 20:35:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// ПЕРЕМЕННЫЕ
+//alert("Привет мир!");
+
+var message; // a-z, A-Z, 0-9, $,
+var myMessage = "Message";
+
+var myNumber = 10.484;
+var myString = "Hello, Alex!";
+var myBoolean = true; // false
+var myNull = null;
+var myUndefined = undefined;
+var myObject = {
+	name: "Alex",
+	age: 49
+};
+
+// ЧИСЛА
+
+console.log(40 + myNumber)	
+console.log(40 - myNumber)
+console.log(40 * myNumber)
+console.log(40 / myNumber)
